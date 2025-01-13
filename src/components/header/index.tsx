919d727b5d9dee8fe4b84e69b4b14f64d06220ce
Ana Sena <luiza1fofa@gmail.com>
2025-01-13 10:54:21 -0300

    loginpage coding

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,21 +1,34 @@
 import { VscAccount } from "react-icons/vsc";
-import { HeaderContainer, Logo, LogInButton, HeaderColor, LinkStyled } from "./style";
+import {
+  HeaderContainer,
+  Logo,
+  LogInButton,
+  HeaderColor,
+  LinkStyled,
+} from "./style";
+import LoginPage from "../loginpage";
 
-const Header = () =>{
-  return(
-    <HeaderColor>
-      <HeaderContainer>      
-        <Logo src="src\assets\logo_bookfy-removebg-preview.png" alt="logo bookfy" />
-        <LinkStyled to="/novo-livro">Novo Livro</LinkStyled>
-        <LinkStyled to="/minha-estante">Minha Estante</LinkStyled>
-        <LogInButton>
-          <div>
-            <VscAccount/>Log In
-        </div>
-        </LogInButton>
-      </HeaderContainer>
-
-    </HeaderColor>
-  )
-}
-export default Header
+const Header = () => {
+  return (
+    <>
+      <LoginPage />
+      <HeaderColor>
+        <HeaderContainer>
+          <Logo
+            src="src\assets\logo_bookfy-removebg-preview.png"
+            alt="logo bookfy"
+          />
+          <LinkStyled to="/novo-livro">Novo Livro</LinkStyled>
+          <LinkStyled to="/minha-estante">Minha Estante</LinkStyled>
+          <LogInButton>
+            <div>
+              <VscAccount />
+              Log In
+            </div>
+          </LogInButton>
+        </HeaderContainer>
+      </HeaderColor>
+    </>
+  );
+};
+export default Header;
---
+++ src/components/loginpage/index.tsx
@@ -0,0 +1,12 @@
+const LoginPage = () => {
+  return(
+    <div>
+      <form action="">
+        <input type="text" name="username" placeholder="Usuário"/>
+        <input type="password" name="pass" placeholder="Senha" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
+export default LoginPage
